2a03c2415ff285922f43ff2177785633b354c54d
Hossam-Abdou <hossam.abdou.almasry@gmail.com>
2023-06-07 23:01:48 +0300

    last

--- lib/components/questions.dart
+++ lib/components/questions.dart
@@ -162,15 +162,54 @@ List<QuestionModel> questionsList2  = [
 List<QuestionModel> questionsList3 = [
   QuestionModel(
     question: "Which planet is the hottest in the solar system?",
-    answer: 0,
+    answer: 1,
     options: ['Earth', 'Venus', 'Jupiter','Saturn'],
   ),
   QuestionModel(
     question: "How many molecules of oxygen does ozone have?",
-    answer: 0,
+    answer: 2,
     options: ['1', '2', '3','5'],
   ),
-
+  QuestionModel(
+    question: "What is the symbol for potassium?",
+    answer: 3,
+    options: ['N', 'S ', 'P', 'K'],
+  ),
+  QuestionModel(
+    question: "What is flutter",
+    answer: 0,
+    options: ['CrossPlatform', 'programming language'],
+  ),
+  QuestionModel(
+    question: "what is the task",
+    answer: 0,
+    options: ['quiz app', 'drawing', 'todo app'],
+  ),
+  QuestionModel(
+    question: "Dart is a programming language?",
+    answer: 0,
+    options: ['True','False'],
+  ),
+  QuestionModel(
+    question: "Who developed the Flutter Framework and continues to maintain it today?",
+    answer: 1,
+    options: ['Facebook','Google','Oracle','Microsoft'],
+  ),
+  QuestionModel(
+    question: "How many types of widgets are there in Flutter?",
+    answer: 3,
+    options: ['0','1','infinity','2'],
+  ),
+  QuestionModel(
+    question: "Access to a cloud database through Flutter is available through which service?",
+    answer: 1,
+    options: ['Sqlite','Firebase Database','NoSql'],
+  ),
+  QuestionModel(
+    question: "What type of test can examine your code as a complete system?",
+    answer: 2,
+    options: [' Unit tests','Widget tests','Integration Tests','All of the above'],
+  ),
 ];
 
 
