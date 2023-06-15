79f51f576c18c51bd6a15d41a2b36f09ff294e3e
Hossam-Abdou <hossam.abdou.almasry@gmail.com>
2023-06-15 19:47:09 +0300

    last commmit

--- lib/components/questions.dart
+++ lib/components/questions.dart
@@ -2,6 +2,31 @@ import '../model/question_model.dart';
 
 List<QuestionModel> questionsList = [
   QuestionModel(
+    question: "What is flutter",
+    answer: 0,
+    options: ['CrossPlatform', 'programming language'],
+  ),
+  QuestionModel(
+    question: "what is the task",
+    answer: 0,
+    options: ['quiz app', 'drawing', 'todo app'],
+  ),
+  QuestionModel(
+    question: "Dart is a programming language?",
+    answer: 0,
+    options: ['True','False'],
+  ),
+  QuestionModel(
+    question: "Who developed the Flutter Framework and continues to maintain it today?",
+    answer: 1,
+    options: ['Facebook','Google','Oracle','Microsoft'],
+  ),
+  QuestionModel(
+    question: "How many types of widgets are there in Flutter?",
+    answer: 3,
+    options: ['0','1','infinity','2'],
+  ),
+  QuestionModel(
     question: "Which planet is the hottest in the solar system?",
     answer: 1,
     options: ['Earth', 'Venus', 'Jupiter','Saturn'],
@@ -11,10 +36,51 @@ List<QuestionModel> questionsList = [
     answer: 2,
     options: ['1', '2', '3','5'],
   ),
+  QuestionModel(
+    question: "What is the symbol for potassium?",
+    answer: 3,
+    options: ['N', 'S ', 'P', 'K'],
+  ),
+  QuestionModel(
+    question: "Access to a cloud database through Flutter is available through which service?",
+    answer: 1,
+    options: ['Sqlite','Firebase Database','NoSql'],
+  ),
+  QuestionModel(
+    question: "What type of test can examine your code as a complete system?",
+    answer: 2,
+    options: [' Unit tests','Widget tests','Integration Tests','All of the above'],
+  ),
 
 ];
+
 List<QuestionModel> questionsList1 = [
   QuestionModel(
+    question: "What is flutter",
+    answer: 0,
+    options: ['CrossPlatform', 'programming language'],
+  ),
+  QuestionModel(
+    question: "what is the task",
+    answer: 0,
+    options: ['quiz app', 'drawing', 'todo app'],
+  ),
+  QuestionModel(
+    question: "Dart is a programming language?",
+    answer: 0,
+    options: ['True','False'],
+  ),
+  QuestionModel(
+    question: "Who developed the Flutter Framework and continues to maintain it today?",
+    answer: 1,
+    options: ['Facebook','Google','Oracle','Microsoft'],
+  ),
+  QuestionModel(
+    question: "How many types of widgets are there in Flutter?",
+    answer: 3,
+    options: ['0','1','infinity','2'],
+  ),
+  QuestionModel(
     question: "Which planet is the hottest in the solar system?",
     answer: 1,
     options: ['Earth', 'Venus', 'Jupiter','Saturn'],
@@ -23,10 +89,52 @@ List<QuestionModel> questionsList1 = [
     question: "How many molecules of oxygen does ozone have?",
     answer: 2,
     options: ['1', '2', '3','5'],
+  ),
+  QuestionModel(
+    question: "What is the symbol for potassium?",
+    answer: 3,
+    options: ['N', 'S ', 'P', 'K'],
+  ),
+  QuestionModel(
+    question: "Access to a cloud database through Flutter is available through which service?",
+    answer: 1,
+    options: ['Sqlite','Firebase Database','NoSql'],
+  ),
+  QuestionModel(
+    question: "What type of test can examine your code as a complete system?",
+    answer: 2,
+    options: [' Unit tests','Widget tests','Integration Tests','All of the above'],
   ),
 
+
 ];
+
 List<QuestionModel> questionsList2 = [
+  QuestionModel(
+    question: "What is flutter",
+    answer: 0,
+    options: ['CrossPlatform', 'programming language'],
+  ),
+  QuestionModel(
+    question: "what is the task",
+    answer: 0,
+    options: ['quiz app', 'drawing', 'todo app'],
+  ),
+  QuestionModel(
+    question: "Dart is a programming language?",
+    answer: 0,
+    options: ['True','False'],
+  ),
+  QuestionModel(
+    question: "Who developed the Flutter Framework and continues to maintain it today?",
+    answer: 1,
+    options: ['Facebook','Google','Oracle','Microsoft'],
+  ),
+  QuestionModel(
+    question: "How many types of widgets are there in Flutter?",
+    answer: 3,
+    options: ['0','1','infinity','2'],
+  ),
   QuestionModel(
     question: "Which planet is the hottest in the solar system?",
     answer: 1,
@@ -36,12 +144,52 @@ List<QuestionModel> questionsList2 = [
     question: "How many molecules of oxygen does ozone have?",
     answer: 2,
     options: ['1', '2', '3','5'],
+  ),
+  QuestionModel(
+    question: "What is the symbol for potassium?",
+    answer: 3,
+    options: ['N', 'S ', 'P', 'K'],
+  ),
+  QuestionModel(
+    question: "Access to a cloud database through Flutter is available through which service?",
+    answer: 1,
+    options: ['Sqlite','Firebase Database','NoSql'],
+  ),
+  QuestionModel(
+    question: "What type of test can examine your code as a complete system?",
+    answer: 2,
+    options: [' Unit tests','Widget tests','Integration Tests','All of the above'],
   ),
 
 ];
 
 List<QuestionModel> questionsList3 = [
   QuestionModel(
+    question: "What is flutter",
+    answer: 0,
+    options: ['CrossPlatform', 'programming language'],
+  ),
+  QuestionModel(
+    question: "what is the task",
+    answer: 0,
+    options: ['quiz app', 'drawing', 'todo app'],
+  ),
+  QuestionModel(
+    question: "Dart is a programming language?",
+    answer: 0,
+    options: ['True','False'],
+  ),
+  QuestionModel(
+    question: "Who developed the Flutter Framework and continues to maintain it today?",
+    answer: 1,
+    options: ['Facebook','Google','Oracle','Microsoft'],
+  ),
+  QuestionModel(
+    question: "How many types of widgets are there in Flutter?",
+    answer: 3,
+    options: ['0','1','infinity','2'],
+  ),
+  QuestionModel(
     question: "Which planet is the hottest in the solar system?",
     answer: 1,
     options: ['Earth', 'Venus', 'Jupiter','Saturn'],
@@ -50,6 +198,21 @@ List<QuestionModel> questionsList3 = [
     question: "How many molecules of oxygen does ozone have?",
     answer: 2,
     options: ['1', '2', '3','5'],
+  ),
+  QuestionModel(
+    question: "What is the symbol for potassium?",
+    answer: 3,
+    options: ['N', 'S ', 'P', 'K'],
+  ),
+  QuestionModel(
+    question: "Access to a cloud database through Flutter is available through which service?",
+    answer: 1,
+    options: ['Sqlite','Firebase Database','NoSql'],
+  ),
+  QuestionModel(
+    question: "What type of test can examine your code as a complete system?",
+    answer: 2,
+    options: [' Unit tests','Widget tests','Integration Tests','All of the above'],
   ),
 
 ];
